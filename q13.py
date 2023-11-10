146d52cd8b3d091b12f62dac1c5427a4094ec6de
Pedroff36 <147555839+Pedroff36@users.noreply.github.com>
2023-11-10 08:03:01 -0300

    ryr

---
+++ q13.py
@@ -0,0 +1,17 @@
+n1=int(input('digite um numero referente a semana 1 a 7'))
+if n1==1:
+    print('domingo')
+elif n1==2:
+    print('segunda')
+elif n1==3:
+    print('terça')
+elif n1==4:
+    print('qaurta')
+elif n1==5:
+    print('quinta')
+elif n1==6:
+    print('sexta')  
+elif n1==7:
+    print('sabado')
+else:
+    print('dia nao conrrespondente')
